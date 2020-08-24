e2d92a3e1705ed61e83fdd616bb33f164e27e5bb
Dmitry Sirotkin <sirotkin.dmitry@yahoo.com>
2020-08-24 13:09:12 +0000

    Delete build_vim.sh

--- build_vim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#copy configs
-cp ./.vimrc ~/.vimrc
-
-#install plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2>/dev/null
-vim +PluginInstall +qall
